29c71a821e402a45151f2670ffd0a69a733927c9
Frans van Genesen <fransvangenesen@gmail.com>
2021-11-02 08:17:24 +0100

    November 2, 2021 - first issue

---
+++ pred_drop_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+"""
+This module contains all classes required to perform database operations.
+"""
+
+
+# constants - consider moving this section to the user interface.
+DB_NAME = "pres_drop.db"
+
+
+class StartDb:
+    """
+    This class contains functions to initiate a fresh database and populate it with the required tables.
+    The functions can be used to refresh the database, in case of corrupted data.
+    """
+    pass
